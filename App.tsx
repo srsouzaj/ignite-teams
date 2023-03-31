a358cc42cd0cc1f718b5c23c0a54170d81433a6d
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-31 14:57:44 -0300

    feat: add interface NewGroup

--- App.tsx
+++ App.tsx
@@ -3,21 +3,24 @@ import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
-import { Groups } from '@screens/Groups';
-import { Loading } from '@components/Loading';
 
+import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <NewGroup /> : <Loading />}
+      </>
     </ThemeProvider>
   );
 }
