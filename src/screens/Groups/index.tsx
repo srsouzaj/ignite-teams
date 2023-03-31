b4f69f935307f853f8817e5dfc46d3c2874bf8b9
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-31 14:31:11 -0300

    feat: add groupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,19 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
-import { Container } from './styles';
+import { Highlight } from '@components/Highlight';
 
+import { Container } from './styles';
 export function Groups() {
     return (
         <Container>
             <Header />
+            <Highlight
+                title="Turmas"
+                subtitle="jogue com sua turma"
+            />
+
+            <GroupCard
+                title="Galera do Ignite" />
         </Container>
     );
 }
