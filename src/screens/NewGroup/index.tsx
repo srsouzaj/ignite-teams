d1ceeadb4666ea719e43d376e6591dd944419aad
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-31 15:06:22 -0300

    feat: add placeholder

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,7 +3,6 @@ import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-
 export function NewGroup() {
     return (
         <Container>
@@ -16,7 +15,9 @@ export function NewGroup() {
                     subtitle="crie a turma para adicionar as pessoas"
                 />
 
-                <Input />
+                <Input
+                    placeholder="Nome da turma"
+                />
 
                 <Button
                     title="Criar"
@@ -25,4 +26,4 @@ export function NewGroup() {
             </Content>
         </Container>
     )
-}
+}   
