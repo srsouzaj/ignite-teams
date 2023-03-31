2666a15e387d97c6f53ffd771478866c45de5276
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-31 14:03:49 -0300

    feat: add highlight

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,21 @@
-import { Container, Logo } from "./styles";
+import { BackButton, BackIcon, Container, Logo } from "./styles";
 
 import logoImg from '@assets/logo.png';
 
-export function Header() {
+interface HeadingInterface {
+    showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: HeadingInterface) {
     return (
         <Container>
+            {
+                showBackButton &&
+                <BackButton>
+                    <BackIcon />
+                </BackButton>
+            }
+
             <Logo source={logoImg} />
         </Container>
     )
