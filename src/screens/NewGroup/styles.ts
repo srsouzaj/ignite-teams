c530f71a785ab5d1da80d7fd80af3ae177ddd3cd
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-03-31 15:02:47 -0300

    feat: add input component

--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -13,8 +13,8 @@ export const Content = styled.View`
 `;
 
 export const Icon = styled(UsersThree).attrs(({ theme }) => ({
-    size: 56,
-    color: theme.COLORS.GREEN_700
+  size: 56,
+  color: theme.COLORS.GREEN_700
 }))`
   align-self: center;
 `;
