fef7a2f0af4d6945e49720a7f95039fb64551463
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-01 21:36:54 -0300

    feat: add playerCard Component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native'
-
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
+import { PlayerCard } from '@components/PlayerCard';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
     const [team, setTeam] = useState('Time A')
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState(['Jorge', 'Roney'])
 
     return (
         <Container>
@@ -29,7 +29,6 @@ export function Players() {
                     icon="add"
                 />
             </Form>
-
             <HeaderList>
                 <FlatList
                     data={['Time A', 'Time B']}
@@ -43,11 +42,21 @@ export function Players() {
                     )}
                     horizontal
                 />
-
                 <NumberOfPlayers>
                     {players.length}
                 </NumberOfPlayers>
             </HeaderList>
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <PlayerCard
+                        name={item}
+                        onRemove={() => { }}
+                    />
+                )}
+            />
         </Container>
     )
 }
