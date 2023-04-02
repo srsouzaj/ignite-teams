3bc44bd55ef7036f60529a65115f5be3aa7499c7
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-01 21:44:46 -0300

    feat: add players

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,6 +1,7 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
 import styled, { css } from "styled-components/native";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;
@@ -13,14 +14,12 @@ export const Form = styled.View`
   justify-content: center;
   border-radius: 6px;
 `;
-
 export const HeaderList = styled.View`
   width: 100%;
   flex-direction: row;
   align-items: center;
   margin: 32px 0 12px;
 `;
-
 export const NumberOfPlayers = styled.Text`
   ${({ theme }) => css`
     color: ${theme.COLORS.GRAY_200};
