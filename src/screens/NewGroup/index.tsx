fabb62ff8457803cf0d2c54b63fdfb793ba4f366
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-01 21:43:46 -0300

    feat: add goBack

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,9 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
+
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+
 export function NewGroup() {
+
+    const navigation = useNavigation()
+
+    function handleNew() {
+        navigation.navigate('players', { group: 'Rocket' })
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -14,16 +25,15 @@ export function NewGroup() {
                     title="Nova turma"
                     subtitle="crie a turma para adicionar as pessoas"
                 />
-
                 <Input
                     placeholder="Nome da turma"
                 />
-
                 <Button
                     title="Criar"
                     style={{ marginTop: 20 }}
+                    onPress={handleNew}
                 />
             </Content>
         </Container>
     )
-}   
+}
