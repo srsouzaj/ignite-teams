54cdb5165e861b5f395efa0254bfc0fd0592df59
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-01 21:42:15 -0300

    feat: add routes types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,9 +5,7 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
-
 import { Container } from './styles';
-
 export function Groups() {
     const [groups, setGroups] = useState<string[]>([]);
     return (
@@ -33,6 +31,7 @@ export function Groups() {
 
             <Button
                 title='Criar nova turma'
+                onPress={() => { }}
             />
         </Container>
     );
