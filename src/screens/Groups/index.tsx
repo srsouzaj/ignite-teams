b71c98c525afe0626fada199ad2a72dde3c451b8
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 15:32:52 -0300

    fix: localstorage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,21 +1,20 @@
 import { useState, useCallback } from 'react';
-
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 
 import { useNavigation, useFocusEffect } from '@react-navigation/native'
 
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
-import { groupsGetAll } from '@storage/group/groupsGetAll';
-
 import { Container } from './styles';
-
+import { Loading } from '@components/Loading';
 
 export function Groups() {
+    const [isLoading, setIsLoading] = useState(true);
     const [groups, setGroups] = useState<string[]>([]);
 
     const navigation = useNavigation();
@@ -26,9 +25,12 @@ export function Groups() {
 
     async function fetchGroups() {
         try {
+            setIsLoading(true);
             const data = await groupsGetAll();
             setGroups(data)
+            setIsLoading(false);
         } catch (error) {
+            Alert.alert('Turmas', 'Não foi possível carregar as turmas');
             console.log(error);
         }
     }
@@ -48,20 +50,24 @@ export function Groups() {
                 title="Turmas"
                 subtitle="jogue com sua turma"
             />
-            <FlatList
-                data={groups}
-                keyExtractor={item => item}
-                renderItem={({ item }) => (
-                    <GroupCard
-                        title={item}
-                        onPress={() => handleOpenGroup(item)}
+            {
+                isLoading ? <Loading /> :
+                    <FlatList
+                        data={groups}
+                        keyExtractor={item => item}
+                        renderItem={({ item }) => (
+                            <GroupCard
+                                title={item}
+                                onPress={() => handleOpenGroup(item)}
+                            />
+                        )}
+                        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                        ListEmptyComponent={() => (
+                            <ListEmpty message="Que tal cadastrar a primeira turma?" />
+                        )}
                     />
-                )}
-                contentContainerStyle={groups.length === 0 && { flex: 1 }}
-                ListEmptyComponent={() => (
-                    <ListEmpty message="Que tal cadastrar a primeira turma?" />
-                )}
-            />
+            }
+
             <Button
                 title='Criar nova turma'
                 onPress={handleNewGroup}
