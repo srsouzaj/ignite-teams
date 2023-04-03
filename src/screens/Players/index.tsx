0d90a24d9b3ff59c922039c3fd68d307eb336386
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-04-03 15:19:15 -0300

    feat: listing teams

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FlatList, Alert } from 'react-native'
+import { useRoute } from '@react-navigation/native';
+
+import { AppError } from '@utils/AppError';
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
@@ -8,117 +14,112 @@ import { Input } from "@components/Input";
 import { PlayerCard } from '@components/PlayerCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
-
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
-import { useRoute } from '@react-navigation/native';
-import { AppError } from '@utils/AppError';
-import { playerAddByGroup } from '@storage/player/playerAddByGroup';
-import { playersGetByGroup } from '@storage/player/playersGetByGroup';
-import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
+
 
 interface RouteParamsInterface {
-    group: string;
+  group: string;
 }
-
 export function Players() {
-    const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState([]);
-    const [newPlayerName, setNewPlayerName] = useState('');
+  const [newPlayerName, setNewPlayerName] = useState('');
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
-    const route = useRoute()
+  const route = useRoute();
 
-    const { group } = route.params as RouteParamsInterface
+  const { group } = route.params as RouteParamsInterface;
 
-    async function handleAddPlayer() {
-        if (newPlayerName.trim().length === 0) {
-            return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
-        }
-
-        const newPlayer = {
-            name: newPlayerName,
-            team,
-        }
-
-        try {
-            await playerAddByGroup(newPlayer, group);
-            const players = await playersGetByGroup(group);
-
-            console.log(players);
-        } catch (error) {
-            if (error instanceof AppError) {
-                Alert.alert('Nova pessoa', error.message);
-            } else {
-                console.log(error);
-                Alert.alert('Nova pessoa', 'Não foi possível adicionar.')
-            }
-        }
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.');
+    }
+    const newPlayer = {
+      name: newPlayerName,
+      team,
     }
 
-    async function fetchPlayersByTeam() {
-        try {
-            const playersByTeam = await playersGetByGroupAndTeam(group, team);
-            setPlayers(playersByTeam)
-        } catch (error) {
-            console.log(error);
-            Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
-        }
+    try {
+      await playerAddByGroup(newPlayer, group);
+      await fetchPlayersByTeam();
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message);
+      } else {
+        console.log(error);
+        Alert.alert('Nova pessoa', 'Não foi possível adicionar.');
+      }
     }
+  }
 
-    return (
-        <Container>
-            <Header showBackButton />
-            <Highlight
-                title={group}
-                subtitle="adicione a galera e separe os times"
-            />
-            <Form>
-                <Input
-                    placeholder="Nome da pessoa"
-                    autoCorrect={false}
-                    onChangeText={setNewPlayerName}
-                />
-                <ButtonIcon
-                    icon="add"
-                    onPress={handleAddPlayer}
-                />
-            </Form>
-            <HeaderList>
-                <FlatList
-                    data={['Time A', 'Time B']}
-                    keyExtractor={item => item}
-                    renderItem={({ item }) => (
-                        <Filter
-                            title={item}
-                            isActive={item === team}
-                            onPress={() => setTeam(item)}
-                        />
-                    )}
-                    horizontal
-                />
-                <NumberOfPlayers>
-                    {players.length}
-                </NumberOfPlayers>
-            </HeaderList>
-            <FlatList
-                data={players}
-                keyExtractor={item => item}
-                renderItem={({ item }) => (
-                    <PlayerCard
-                        name={item}
-                        onRemove={() => { }}
-                    />
-                )}
-                ListEmptyComponent={() => (
-                    <ListEmpty message="Não há pessoas nesse time" />
-                )}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
-            />
+  async function fetchPlayersByTeam() {
+    try {
+      const playersByTeam = await playersGetByGroupAndTeam(group, team);
+      setPlayers(playersByTeam);
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
+    }
+  }
 
-            <Button
-                title="Remover Turma"
-                type="SECONDARY"
+  useEffect(() => {
+    fetchPlayersByTeam();
+  }, [team])
+
+  return (
+    <Container>
+      <Header showBackButton />
+      <Highlight
+        title={group}
+        subtitle="adicione a galera e separe os times"
+      />
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+          onChangeText={setNewPlayerName}
+        />
+        <ButtonIcon
+          icon="add"
+          onPress={handleAddPlayer}
+        />
+      </Form>
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
             />
-        </Container>
-    )
+          )}
+          horizontal
+        />
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item.name}
+        renderItem={({ item }) => (
+          <PlayerCard
+            name={item.name}
+            onRemove={() => { }}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+      />
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
+      />
+    </Container>
+  )
 }
